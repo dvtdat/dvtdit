f46c3a7007ed1d4033fe43fc7ebc27d01dc672f0
dvtdat <tiendat0417@gmail.com>
2025-06-27 23:19:06 +0700

    feat: object file creation

--- src/commands/commit.cpp
+++ src/commands/commit.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
@@ -6,7 +9,10 @@
 #include <iomanip> 
 
 #include "../utils/types.h"
+#include "../utils/hash.h"
+#include "../utils/file_utils.h"
 #include "../utils/index_utils.h"
+#include "../utils/blob.h"
 
 void handleCommit(const Arguments& args) {
   std::map<std::string, FileMetadata> indexes = index_utils::readIndex();
@@ -27,5 +33,26 @@ void handleCommit(const Arguments& args) {
     message = args.flagsWithValue.at("--message"); 
   }
 
-  std::cout << message << '\n';
+  for (std::map<std::string, FileMetadata>::const_iterator it = indexes.begin(); it != indexes.end(); ++it) {
+    const std::string& filename = it->first;
+    const FileMetadata& metadata = it->second;
+
+    std::string fileData = file_utils::readFile(filename);
+    std::string fileBlob = blob::convertToBlob(fileData);
+    std::string hashedData = hash::hashDataSHA1(fileBlob);
+
+    std::string objectDirectory = ".dit/objects/" + hashedData.substr(0, 2);
+    std::string objectFilename = objectDirectory + "/" + hashedData.substr(2);
+
+    std::cout << objectFilename << '\n';
+
+    std::filesystem::create_directory(objectDirectory); 
+    std::ofstream file(objectFilename);
+    if (file.is_open()) {
+        file << "This is the commit message: " << message;
+        file.close();
+    } else {
+        std::cerr << "Failed to create file: " << objectFilename << '\n';
+    }
+  }
 }
--- src/commands/init.cpp
+++ src/commands/init.cpp
@@ -3,23 +3,15 @@
 #include <fstream>
 
 void handleInit() {
-  namespace fs = std::filesystem;
-  
-  if (fs::exists(".dit")) {
+  if (std::filesystem::exists(".dit")) {
     std::cout << "Repository already initialized.\n";
   }
 
-  try {
-    fs::create_directory(".dit");
-    fs::create_directory(".dit/objects");
-    fs::create_directory(".dit/refs");
+  std::filesystem::create_directory(".dit");
+  std::filesystem::create_directory(".dit/objects");
+  std::filesystem::create_directory(".dit/refs");
 
-    std::ofstream head(".dit/HEAD");
-    head << "ref: refs/heads/main\n";
-    head.close();
-  }
-  catch (const std::exception &e)
-  {
-    std::cerr << "Error initializing repository: " << e.what() << '\n';
-  }
+  std::ofstream head(".dit/HEAD");
+  head << "ref: refs/heads/main\n";
+  head.close();
 }
